25c6b8c593739050c2431661de26c8bc50b3dfbc
NickJacksonpyah <s137665@gmail.com>
2014-10-27 14:07:27 -0500

    85ptlab9
    i was wondering why i said if it is greater than 100 it still print
    all.. i was wondering why

--- lab9-85pt.py
+++ lab9-85pt.py
@@ -11,6 +11,16 @@ myList = [102,98,96,101,100,99,103,97,98,105]
 
 # Insert for loop here
 
+############################################
 
+# Create a for loop that will search through temperatureList
+# and create a new list that includes only numbers greater than 100
+
+myList = [102,98,96,101,100,99,103,97,98,105]
+
+# Insert for loop here
+
+if myList > 100:
 # This should print [102,101,103,105]
-print myList
+        print myList
+
